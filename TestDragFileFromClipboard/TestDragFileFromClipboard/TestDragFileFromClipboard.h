e12fc26a6f2943718fd7c9584cc632743be555ba
dudugi <du_chen_hi@126.com>
2019-07-12 17:51:32 +0800

    TestDragFileFromClipboard

---
+++ TestDragFileFromClipboard/TestDragFileFromClipboard/TestDragFileFromClipboard.h
@@ -0,0 +1,32 @@
+
+// TestDragFileFromClipboard.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CTestDragFileFromClipboardApp:
+// 有关此类的实现，请参阅 TestDragFileFromClipboard.cpp
+//
+
+class CTestDragFileFromClipboardApp : public CWinApp
+{
+public:
+	CTestDragFileFromClipboardApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTestDragFileFromClipboardApp theApp;
